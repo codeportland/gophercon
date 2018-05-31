7129bfdf1605a6ca5bbbba9e225d1d2f49c2139c
Rebecca Wise <rebecca@redshift.is>
2018-05-31 11:17:38 +0000

    add config

--- cmd/gophercon/gophercon.go
+++ cmd/gophercon/gophercon.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/ccp-codex/gophercon/pkg/routing"
 )
@@ -11,7 +12,14 @@ import (
 func main() {
 	log.Printf("Service is starting...")
 
+	// todo: you can also use github.com/kelsey/hightower/envconfig
+	// to keep your config more structured
+	port := os.Getenv("SERVICE_PORT")
+	if len(port) == 0 {
+		log.Fatal("Service port was not defined")
+	}
+
 	r := routing.BaseRouter()
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
